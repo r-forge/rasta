68d307880ce5887a1bc114ce177bf3311161d999
janvb <janvb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-09-21 19:48:57 +0000

    rasta preliminary work
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rasta@9 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/rasta/R/mysummary.R
@@ -0,0 +1,21 @@
+## a demo function
+
+# function example - get measures of central tendency
+# and spread for a numeric vector x. The user has a
+# choice of measures and whether the results are printed.
+
+mysummary <- function(x,npar=TRUE,print=TRUE) {
+  if (!npar) {
+    center <- mean(x); spread <- sd(x) 
+  } else {
+    center <- median(x); spread <- mad(x) 
+  }
+  if (print & !npar) {
+    cat("Mean=", center, "\n", "SD=", spread, "\n")
+  } else if (print & npar) {
+    cat("Median=", center, "\n", "MAD=", spread, "\n")
+  }
+  result <- list(center=center,spread=spread)
+  return(result)
+}
+
