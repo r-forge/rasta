492fa88c07cf74cea6d9bc043890f9177aa2bef4
dutri001 <dutri001@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-09-26 12:45:55 +0000

    Bug fix on QA2cloud function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rasta@33 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/rasta/R/QA2cloud.R
+++ pkg/rasta/R/QA2cloud.R
@@ -1,8 +1,8 @@
 # Author: Loic Dutrieux
 # September 2013
 
-QA2cloud <- function(x, bitpos=0x8000) {
-  if (bitAnd(x, bitpos) != 0) {
+QA2cloud <- function(x, bitpos=0xC000) {
+  if (bitAnd(x, bitpos) == bitpos) {
     cloud <- 1
   } else {
     cloud <- 0
