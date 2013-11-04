dc5b03a37b809494d254f0fac525e9a52c448426
janvb (Dr. ) <Janverbesselt@gmail.com>
2013-11-04 11:18:31 +0000

    minor update
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rasta@187 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/howtousetherastapackage.R
+++ pkg/howtousetherastapackage.R
@@ -1,5 +1,7 @@
 library(rasta)
+
 ?mysummary
-# demo("Lesson_1_demo", package = "rasta")
+
+demo("Lesson_1_demo", package = "rasta")
 getwd()
 download.file('http://rasta.r-forge.r-project.org/kroonven.csv', 'kroonven.csv')
